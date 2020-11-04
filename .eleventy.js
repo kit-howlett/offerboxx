1949ea6272f89167c9a3c16a7ebe407bafd02ab0
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2020-11-04 18:48:39 +0000

    Set up the basis for individual listing pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,20 @@
+// Filters
+const dateFilter = require('./src/filters/date-filter.js');
+const w3DateFilter = require('./src/filters/w3-date-filter.js');
+
 module.exports = config => {
+  // Set directories to pass through to the dist folder
+  config.addPassthroughCopy('./src/images/');
+
+  // Add filters
+  config.addFilter('dateFilter', dateFilter);
+  config.addFilter('w3DateFilter', w3DateFilter);
+
+  // Returns a collection of listings in reverse date order
+  config.addCollection('listings', collection => {
+    return [...collection.getFilteredByGlob('./src/listings/*.md')].reverse();
+  });
+
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
