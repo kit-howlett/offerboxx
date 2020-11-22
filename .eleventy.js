1f78b34fbca9859e41cf7e9d22e5d204b4c3cb4a
Patrick Kelly <patrick@adaptdigital.co>
2020-11-22 19:13:35 +0000

    Add categories, locations & articles collections

--- .eleventy.js
+++ .eleventy.js
@@ -15,7 +15,19 @@ module.exports = config => {
 
   // Returns a collection of listings in reverse date order
   config.addCollection('listings', collection => {
-    return [...collection.getFilteredByGlob('./src/listings/*.md')].reverse();
+    return [...collection.getFilteredByGlob('./src/content/listings/*.md')].reverse();
+  });
+  
+  config.addCollection('categories', collection => {
+    return [...collection.getFilteredByGlob('./src/content/categories/*.md')].reverse();
+  });
+  
+  config.addCollection('locations', collection => {
+    return [...collection.getFilteredByGlob('./src/content/locations/*.md')].reverse();
+  });
+  
+  config.addCollection('articles', collection => {
+    return [...collection.getFilteredByGlob('./src/content/articles/*.md')].reverse();
   });
 
   return {
